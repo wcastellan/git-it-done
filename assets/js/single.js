fbc8c518460420c6c462b63c669b73e396aadc3b
Wyatt C <wcastellan@yahoo.com>
2021-11-10 14:21:04 -0500

    added feature/ list issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,24 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+
+
+}
 
 var displayWarning = function(repo) {
     // add text to warning container
@@ -70,9 +88,10 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
